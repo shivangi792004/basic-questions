5c316a2aff8486c9ce1442fbee22aff0635f2ad5
shivangi792004 <shivangisingh8688@gmail.com>
2024-06-11 22:20:31 +0530

    Add files via upload

---
+++ area_of_rectangle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int length = 12;
+    int breadth =14;
+    int area_reactangle;
+    area_reactangle =length*breadth;
+    cout<<"Area of rectangele is: "<<area_reactangle<<endl;
+    return 0;
+}
