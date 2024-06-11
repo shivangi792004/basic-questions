2c3a7e3dec56b15e7bd952d0d0f9e170c75488cc
shivangi792004 <shivangisingh8688@gmail.com>
2024-06-11 22:20:06 +0530

    Add files via upload

---
+++ area_of_circumference.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int radius=2;
+    float pi=3.14;
+
+    float area=2*(pi*radius);
+    cout<<"Area ofcircumference: "<<area;
+    return 0;
+}
