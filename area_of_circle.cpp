96ad2ad3e57f490fa58e23af8312415c93f1ea88
shivangi792004 <shivangisingh8688@gmail.com>
2024-06-11 22:19:44 +0530

    Add files via upload

---
+++ area_of_circle.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int pi=3.14;
+    int radius=3;
+
+    int area;
+    area = pi *(radius*radius);
+    cout<<"Area of square is: "<<area<<endl;
+    return 0;
+}
