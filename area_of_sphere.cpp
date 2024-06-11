e92365a077cd11d68af4f689114b2d6c2d3d76e7
shivangi792004 <shivangisingh8688@gmail.com>
2024-06-11 22:20:40 +0530

    Add files via upload

---
+++ area_of_sphere.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float pi=3.14;
+    int radius=3;
+
+    float area=4*(pi*radius*radius);
+    cout<<"Area of sphere: "<<area;
+    return 0;
+}
